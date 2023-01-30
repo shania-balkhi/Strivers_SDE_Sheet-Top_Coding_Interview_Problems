0090a4e89103b6c3c62c260888c434ed6c1b31c3
Shania Balkhi <balkhi.shania@gmail.com>
2023-01-30 09:18:28 +0530

    Update Q1SetMatrixZeroes.java

--- A. Arrays/Q1SetMatrixZeroes.java
+++ A. Arrays/Q1SetMatrixZeroes.java
@@ -1,3 +1,4 @@
+//approach 1
 //brute force method
 //TC : (N x M ) x (N + M)
        // (N x M) for linearly traversing in the array
@@ -30,6 +31,36 @@ class Solution {
         for(int i = 0; i < matrix.length; ++i){
             for(int j = 0; j < matrix[0].length; ++j){
                 if(matrix[i][j] == Integer.MIN_VALUE + 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+// **********************************************************************************************************
+
+//approach 2
+//striver approach 2 --- better approach --- works!
+//TC : O((N x M) + (N x M)) ; because we linearly traversed the array twice
+//SC : O(N) + O(M) ; for taking two dummy arrays
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int[] dcol = new int[matrix[0].length];
+        int[] drow = new int[matrix.length];
+
+        for(int i = 0; i < matrix.length; ++i){
+            for(int j = 0; j < matrix[i].length; ++j){
+                if(matrix[i][j] == 0){
+                    dcol[j] = -1;
+                    drow[i] = -1;
+                }
+            }
+        }
+
+        for(int i = 0; i < matrix.length; ++i){
+            for(int j = 0; j < matrix[i].length; ++j){
+                if(dcol[j] == -1 || drow[i] == -1){
                     matrix[i][j] = 0;
                 }
             }
