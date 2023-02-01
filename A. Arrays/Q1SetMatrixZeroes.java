597c08bb79ddfcdedfc9bc2a47243a95dfa5fc94
Shania Balkhi <balkhi.shania@gmail.com>
2023-02-01 22:31:07 +0530

    Update Q1SetMatrixZeroes.java

--- A. Arrays/Q1SetMatrixZeroes.java
+++ A. Arrays/Q1SetMatrixZeroes.java
@@ -67,3 +67,43 @@ class Solution {
         }
     }
 }
+
+// *****************************************************************************************************************
+
+//approach 3
+//striver optimal approach
+//most optimal approach is the optimisation of the better approach (previous approach) that we did
+//in the better approach we took two dummy arrays. now, in this approach instead of taking two dummy arrays explicitly, we will take
+//those two dummy arrays inside the 2d matrix
+// TC : 2 * (N x M) ; because we traverse the matrix twice
+// SC : O(1) ; because the changes done were in-place
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        //intialise col0 = true
+        boolean col0 = true;
+
+        for(int i = 0; i < matrix.length; ++i){
+            if(matrix[i][0] == 0){ //if the 0th column has value 0, then do col0 = false
+                col0 = false;
+            }
+            for(int j = 1; j < matrix[0].length; ++j){ //now check for the rest of the columns
+                if(matrix[i][j] == 0){ //now, if value at any index is 0, then mark its corresponding index in the dummy_Row array and dummy_Column array as 0 
+                    matrix[i][0] = 0; //fill column dummy array
+                    matrix[0][j] = 0; //fill row dummy array
+                }
+            }
+        }
+
+        //traverse backwards in the matrix
+        for(int i = matrix.length - 1; i >= 0; --i){
+            for(int j = matrix[0].length - 1; j >= 1; --j){ //careful, check only till the 1st column (and not till 0th column)
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){ //if the value in either of the dummy arrays is zero, then mark its corresponding index in the given matrix with value 0
+                    matrix[i][j] = 0;
+                }
+            }
+            if(col0 == false){ //if col0 == false, that means the 0th column indices is supposed to have value as 0
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
