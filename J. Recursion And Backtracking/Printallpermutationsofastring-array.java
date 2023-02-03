d0c9b7c6ee7933131a870d0ef75576678f4150ef
Shania Balkhi <balkhi.shania@gmail.com>
2023-02-03 08:34:17 +0530

    Update Printallpermutationsofastring-array.java

--- J. Recursion And Backtracking/Printallpermutationsofastring-array.java
+++ J. Recursion And Backtracking/Printallpermutationsofastring-array.java
@@ -31,3 +31,44 @@ class Solution {
 }
 
 //*********************************************************************************************************************
+
+//striver code 2
+//approach 2
+//SC : n! * n
+// TC : O(1)
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        recPermuteHelper(0, nums, ans);
+        return ans;
+    }
+
+    private void recPermuteHelper(int si, int[] nums, List<List<Integer>> ans){
+        //base case
+        if(si == nums.length){
+            //create and initialise inner smallOutput arrayList
+            List<Integer> ds = new ArrayList<Integer>();
+            for(int i = 0; i < nums.length; ++i){
+                ds.add(nums[i]);
+            }
+
+            //add this smallOutput inner arrayList to outer arrayList
+            ans.add(ds);
+            return;
+        }
+
+        for(int i = si; i < nums.length; ++i){
+            swap(si, i, nums); //replace index element with ith element
+            recPermuteHelper(si + 1, nums, ans);
+            swap(i, si, nums); //replace back both indices
+        } 
+
+    }
+
+    private void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
