f261f15544308d73d788d27efe5a31611b343616
Shania Balkhi <balkhi.shania@gmail.com>
2023-02-04 11:00:53 +0530

    Update Q5SortAnArrayOf0s1s2s.java

--- A. Arrays/Q5SortAnArrayOf0s1s2s.java
+++ A. Arrays/Q5SortAnArrayOf0s1s2s.java
@@ -8,7 +8,7 @@ class Solution {
     }
 }
 
-// **********************************************************
+// **************************************************************************************
 
 //striver code 2
 //approach 2 : counting sort (keeping count of values) --- done in 2 pass
@@ -45,3 +45,33 @@ class Solution {
 }
 
 // ***************************************************************************************
+
+//striver code 3
+//approach 3 : 3 pointer approach (variation of dutch national flag algorithm) --- done in 1 pass
+//TC : O(N)
+// SC : O(1)
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(low, mid, nums);
+                ++low;
+                ++mid;
+            }
+            else if(nums[mid] == 1){
+                ++mid;
+            }
+            else if(nums[mid] == 2){
+                swap(mid, high, nums);
+                --high;
+            }
+        }
+    }
+
+    private void swap(int i, int j, int[] nums){ //i and j are the indices whose values in the array need to be interchanged
+        int temp =nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
